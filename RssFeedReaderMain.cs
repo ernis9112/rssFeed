1b89ee10182582dea89d32f3779e672c6156bbc6
Mazvis <sivzam@gmail.com>
2014-10-02 00:01:05 +0300

    Update RssFeedReaderMain.cs

--- RssFeedReaderMain.cs
+++ RssFeedReaderMain.cs
@@ -15,6 +15,21 @@ namespace RssFeedReader
 
             System.Console.WriteLine(FeedsCounterSingleton.getInstance().getFeeds());
 
+            //Categories
+            NewsPerCategoryCounterSingleton.getInstance().addCategoryAndTitle("namai", "siandien kazkas tipo ale atsitiko");
+            NewsPerCategoryCounterSingleton.getInstance().addCategoryAndTitle("bites", "nieko naujo");
+            NewsPerCategoryCounterSingleton.getInstance().addCategoryAndTitle("bite", "nieko naujo");
+            NewsPerCategoryCounterSingleton.getInstance().addCategoryAndTitle("bite", "nieko naujo");
+            NewsPerCategoryCounterSingleton.getInstance().addCategoryAndTitle("bite", "nieko naujo");
+
+            var categories = NewsPerCategoryCounterSingleton.getInstance().getCategories();
+
+            System.Console.WriteLine("Categories:");
+            foreach (KeyValuePair<string, int> temp in categories)
+            {
+                System.Console.Write("\"" + temp.Key.ToString() + "\" : " + temp.Value.ToString() + "\n");
+            }
+
             //stop program for a while
             Console.ReadLine();
         }
