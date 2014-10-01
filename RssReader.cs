0af31ad5801796603c1bd0ba9d83dc3b27c71686
ernis9112 <ernis9112@gmail.com>
2014-10-01 23:03:04 +0300

    Main part of RSS feed reading

---
+++ RssReader.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RssFeedReader
+{
+    public class RssReader
+    {
+
+        public static void readOneFeed(string url)
+        {
+
+            StringBuilder output = new StringBuilder();
+            using (XmlReader reader = XmlReader.Create(url))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        if (!reader.IsEmptyElement)
+                        {
+
+                            //will be some code for feed data saving
+                            reader.ReadToFollowing("item");
+                            FeedsCounterSingleton.getInstance().addFeeds(1);
+                        }
+                    }
+                }
+            }
+
+            System.Console.WriteLine(FeedsCounterSingleton.getInstance().getFeeds());
+        }
+
+    }
+}
