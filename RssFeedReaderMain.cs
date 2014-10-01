627e9c82ee9ca3a7873c59a85cdac623d3d2654d
ernis9112 <ernis9112@gmail.com>
2014-10-01 23:13:18 +0300

    Some improvements for singleton

--- RssFeedReaderMain.cs
+++ RssFeedReaderMain.cs
@@ -11,7 +11,10 @@ namespace RssFeedReader
         static void Main(string[] args)
         {
             RssReader.readOneFeed("http://www.delfi.lt/rss/feeds/lithuania.xml");
+            System.Console.WriteLine(FeedsCounterSingleton.getInstance().getFeeds());
+
             RssReader.readOneFeed("http://www.15min.lt/rss");
+            System.Console.WriteLine(FeedsCounterSingleton.getInstance().getFeeds());
 
             //stop program for a while
             Console.ReadLine();
