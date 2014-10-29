8d13be5f2a75cc39c9111063bdddf90689b00190
ernis9112 <ernis9112@gmail.com>
2014-10-29 23:23:49 +0200

    Update FilteredFeeds.java

--- FilteredFeeds.java
+++ FilteredFeeds.java
@@ -8,7 +8,7 @@ class FilteredFeeds {
     public FilteredFeeds(FilterType ifilter) {
         this.filter = ifilter;
     }
-    public ArrayList<String> executeFiltering() {
-        return this.filter.filter();
+    public void executeFiltering() {
+        this.filter.filter();
     }
 }
