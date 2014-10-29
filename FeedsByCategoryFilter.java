c26fddb4e8662479ea15e88ae5f66097fe2a706c
ernis9112 <ernis9112@gmail.com>
2014-10-29 20:29:40 +0200

    Create FeedsByCategoryFilter.java

---
+++ FeedsByCategoryFilter.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class FeedsByCategoryFilter implements FilterType {
+	
+	public ArrayList<String> filter(){
+		// get feeds from database
+		return new ArrayList<String>();
+	}
+
+}
