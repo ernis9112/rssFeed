6b8f89b56acb2f6465966221d2c2b31047f30da2
ernis9112 <ernis9112@gmail.com>
2014-10-29 23:23:30 +0200

    Update FeedsByCategoryFilter.java

--- FeedsByCategoryFilter.java
+++ FeedsByCategoryFilter.java
@@ -1,10 +1,22 @@
 import java.util.ArrayList;
 
+
 public class FeedsByCategoryFilter implements FilterType {
 	
-	public ArrayList<String> filter(){
+	private String category;
+	
+	public FeedsByCategoryFilter(String iCategory){
+		this.category = iCategory;
+	}
+	
+	public void filter(){
 		// get feeds from database
-		return new ArrayList<String>();
+		Database db = new Database();
+		try {
+			db.executeQuery("SELECT title from rssfeeds WHERE category = '" + category + "'", "select");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
