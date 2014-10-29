9244595a07d2314a625d284a26fd0beb1a95ca38
ernis9112 <ernis9112@gmail.com>
2014-10-29 23:23:15 +0200

    Update FeedsByAuthorFilter.java

--- FeedsByAuthorFilter.java
+++ FeedsByAuthorFilter.java
@@ -3,9 +3,20 @@ import java.util.ArrayList;
 
 public class FeedsByAuthorFilter implements FilterType {
 	
-	public ArrayList<String> filter(){
+	private String author;
+	
+	public FeedsByAuthorFilter(String iAuthor){
+		this.author = iAuthor;
+	}
+	
+	public void filter(){
 		// get feeds from database
-		return new ArrayList<String>();
+		Database db = new Database();
+		try {
+			db.executeQuery("SELECT title from rssfeeds WHERE author = '" + author + "'", "select");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
