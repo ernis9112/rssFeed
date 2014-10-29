3c617c48e3b88542216c48a8e297d0a8f0870fb3
ernis9112 <ernis9112@gmail.com>
2014-10-29 23:22:59 +0200

    Update Database.java

--- Database.java
+++ Database.java
@@ -2,25 +2,27 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.DatabaseMetaData;
  
 public class Database
 {
 	
-    public void executeQuery(String query) throws Exception
+    public void executeQuery(String query, String type) throws Exception
     {
-        String sDriverName = "org.sqlite.JDBC";
-
         String sTempDb = "database.db";
         String sJdbc = "jdbc:sqlite";
         String sDbUrl = sJdbc + ":" + sTempDb;
 
         int iTimeout = 30;
         String sMakeTable = "CREATE TABLE IF NOT EXISTS rssfeeds (date text, author text, category text, link text, title text, UNIQUE(title))";
-        String sMakeInsert = query;
         
-        String sMakeSelect = "SELECT title from rssfeeds";
+        String sMakeInsert = "";
+        String sMakeSelect = "";
+        
+        if(type.equals("insert"))
+            sMakeInsert = query;
+        
+        if(type.equals("select"))
+            sMakeSelect = query;
  
         // create a database connection
         Connection conn = DriverManager.getConnection(sDbUrl);
@@ -29,16 +31,19 @@ public class Database
             try {
                 stmt.setQueryTimeout(iTimeout);
                 stmt.executeUpdate( sMakeTable );
-                stmt.executeUpdate( sMakeInsert );
-                ResultSet rs = stmt.executeQuery(sMakeSelect);
-                try {
-                    while(rs.next())
+                if(type.equals("insert"))
+                    stmt.executeUpdate( sMakeInsert );
+                if(type.equals("select")){
+                    ResultSet rs = stmt.executeQuery(sMakeSelect);
+                    try {
+                        while(rs.next())
                         {
                             String sResult = rs.getString("title");
                             System.out.println(sResult);
                         }
-                } finally {
-                    try { rs.close(); } catch (Exception ignore) {}
+                    } finally {
+                        try { rs.close(); } catch (Exception ignore) {}
+                    }
                 }
             } finally {
                 try { stmt.close(); } catch (Exception ignore) {}
