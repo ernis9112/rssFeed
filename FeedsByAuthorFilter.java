628b63c78954e2f50ce42f8bc75309333289335e
ernis9112 <ernis9112@gmail.com>
2014-10-29 20:29:19 +0200

    Create FeedsByAuthorFilter.java

---
+++ FeedsByAuthorFilter.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class FeedsByAuthorFilter implements FilterType {
+	
+	public ArrayList<String> filter(){
+		// get feeds from database
+		return new ArrayList<String>();
+	}
+
+}
