80a6629f1667d7b141a37ec21fa983abf41ec24d
ernis9112 <ernis9112@gmail.com>
2014-10-30 01:20:09 +0200

    Create RssReaderBbc.java

---
+++ RssReaderBbc.java
@@ -0,0 +1,15 @@
+
+public class RssReaderBbc extends RssReader {
+	
+	public RssReaderBbc(){
+		this.setName("bbc");
+		this.setHaveCategory(false);
+	}
+
+	@Override
+	public void createReader(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
