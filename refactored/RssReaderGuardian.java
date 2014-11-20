3052280aa34076b99b39641194da820549590f39
ernis9112 <ernis9112@gmail.com>
2014-11-20 10:31:15 +0200

    Create RssReaderGuardian.java

---
+++ refactored/RssReaderGuardian.java
@@ -0,0 +1,15 @@
+
+public class RssReaderGuardian extends RssReader {
+	
+	public RssReaderGuardian(){
+		this.setName("guardian");
+		this.setHaveCategory(false);
+	}
+
+	@Override
+	public void createReader(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
