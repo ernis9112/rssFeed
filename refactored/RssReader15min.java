07068b839407afd06bece18410f8fe5567909a7a
ernis9112 <ernis9112@gmail.com>
2014-11-20 10:29:34 +0200

    Create RssReader15min.java

---
+++ refactored/RssReader15min.java
@@ -0,0 +1,14 @@
+
+public class RssReader15min extends RssReader {
+	
+	public RssReader15min(){
+		this.setName("15min");
+	}
+
+	@Override
+	public void createReader(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
