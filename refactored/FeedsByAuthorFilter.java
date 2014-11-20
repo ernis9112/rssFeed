b9011b10a196a244610925a31120e36f435fd3a7
ernis9112 <ernis9112@gmail.com>
2014-11-20 10:25:47 +0200

    Create FeedsByAuthorFilter.java

---
+++ refactored/FeedsByAuthorFilter.java
@@ -0,0 +1,19 @@
+public class FeedsByAuthorFilter implements FilterType {
+	
+	private String author;
+	
+	public FeedsByAuthorFilter(String iAuthor){
+		this.author = iAuthor;
+	}
+	
+	public void filter() throws Exception{
+		// get feeds from database
+		final IConnection conn = new ProxyConnection();
+		try {
+			conn.executeQuery("author", "select", author);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
