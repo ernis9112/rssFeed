c5dc44a04a07b9b70c71f3798b097caed218822a
ernis9112 <ernis9112@gmail.com>
2014-11-20 10:30:40 +0200

    Create RssReaderDelfi.java

---
+++ refactored/RssReaderDelfi.java
@@ -0,0 +1,14 @@
+
+public class RssReaderDelfi extends RssReader {
+	
+	public RssReaderDelfi(){
+		this.setName("delfi");
+	}
+
+	@Override
+	public void createReader(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
